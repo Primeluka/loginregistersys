a7c78bcd9f30aa52c621aa0c5ccbc41f9141a6ef
lukasz.nedzka <nedzka.lukasz@gmail.com>
2019-02-12 15:43:14 +0100

    Created adding new user to the database

--- register.php
+++ register.php
@@ -29,8 +29,26 @@ if(Input::exists()) {
         ));
 
         if($validation->passed()) {
-            Session::flash('success', 'Your registered successfully!');
-            header('Location: index.php');
+            $user = new User();
+
+            $salt = Hash::salt(32);
+
+            try {
+                $user->create(array(
+                    'username' => Input::get('username'),
+                    'password' => Hash::make(Input::get('password'), $salt),
+                    'salt' => $salt,
+                    'name' => Input::get('name'),
+                    'joined' => date('Y-m-d H:i:s'),
+                    'group' => 1
+                ));
+
+                Session::flash('home', 'You have been registered and can now log in!');
+                Header('Location: index.php');
+
+            } catch (Exception $e) {
+                die($e->getMessage());
+            }
         } else {
             foreach($validation->errors() as $error) {
                 echo $error.'<br>';
